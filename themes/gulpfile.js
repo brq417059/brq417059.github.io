98df6c0d2c13959d784718570bd02c312b652df0
baoruiqi <rdbaoruiqi@gmail.com>
2020-12-17 13:44:04 +0800

    update

---
+++ themes/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var imagemin = require('gulp-imagemin');
+
+// 压缩 html
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'))
+});
+// 压缩 css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩 js
+gulp.task('minify-js', function() {
+    return gulp.src('./public/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+});
+// 默认任务
+gulp.task('default', gulp.parallel(
+    'minify-html','minify-css','minify-js','minify-images'
+));
